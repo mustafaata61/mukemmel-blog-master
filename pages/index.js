99e62405e0768c3e0d1865e868930da67ac7bb8d
mustafaata61 <58668396+mustafaata61@users.noreply.github.com>
2019-12-13 21:00:13 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -131,7 +131,7 @@ const Home = ({ posts }) => (
 
 Home.getInitialProps = async ({ req }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch("http://https://mukemmel-blog-benim.herokuapp.com/api/posts");
   const json = await res.json();
   return { posts: json.posts };
 };
