8434939bd25d0bc22fe09aadca3a6548d1dc49d5
mustafaata61 <58668396+mustafaata61@users.noreply.github.com>
2019-12-13 21:14:29 +0300

    Update iletisim.js

--- pages/iletisim.js
+++ pages/iletisim.js
@@ -98,7 +98,7 @@ const Home = ({ posts }) => (
 
 Home.getInitialProps = async ({ req }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch("https://mukemmel-blog-benim.herokuapp.com/iletisim");
   const json = await res.json();
   return { posts: json.posts };
 };
