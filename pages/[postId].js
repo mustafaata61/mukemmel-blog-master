6117f60e3d297bdf22937914071e94bcff32d6f3
mustafaata61 <58668396+mustafaata61@users.noreply.github.com>
2019-12-13 21:10:47 +0300

    Update [postId].js

--- pages/[postId].js
+++ pages/[postId].js
@@ -125,7 +125,7 @@ const BlogPost = ({ post }) => (
 
 BlogPost.getInitialProps = async ({ req, query }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch(`http://https://mukemmel-blog-benim.herokuapp.com/api/post/${query.postId}`);
+  const res = await fetch(`https://mukemmel-blog-benim.herokuapp.com/api/post/${query.postId}`);
   const json = await res.json();
   return { post: json.post };
 };
